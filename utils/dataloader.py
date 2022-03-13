dd3e2449d0e9d81ad2579bcb22ed506c2a566b4f
Luca Sosta <lucasosta@libero.it>
2022-03-13 12:49:04 +0100

    dataloader

--- utils/dataloader.py
+++ utils/dataloader.py
@@ -1,12 +1,5 @@
 import numpy as np
-import matplotlib.pylab as plt
-from mpl_toolkits.mplot3d import Axes3D
-import os
-
 import tensorflow as tf
-
-import random
-random.seed(20)
 
 class dataloader:
 	"""
@@ -21,8 +14,7 @@ class dataloader:
 		@param batch_size dimension of a batch_size for collocation points
 		@param reshuffle_every_epoch boolean that indicates if we want to reshuffle the points at every epoch
 		"""
-
-		## datasets_class object, we'll use its getter methods(get_coll_data(), get_num_collocation() etc)
+		## datasets_class object, we'll use its getter methods (get_coll_data(), get_num_collocation() etc)
 		self.datasets_class = datasets_class
 		## batch size of collocation points for minibatch training
 		self.batch_size = batch_size
@@ -33,7 +25,7 @@ class dataloader:
 		"""Return a dataloader for collocation points.
 		Implemented using tf.data.Dataset.from_tensor_slices and batch"""
 		# get the collocation data
-		inputs,_,_ = self.datasets_class.get_coll_data()
+		inputs, _, _ = self.datasets_class.get_coll_data()
 
 		#load the data of collocation
 		data = tf.data.Dataset.from_tensor_slices(inputs)
@@ -42,20 +34,18 @@ class dataloader:
 		data = data.shuffle(buffer_size=(self.datasets_class.get_num_collocation()+1),
 							reshuffle_each_iteration=self.reshuffle_each_iteration)
 
+        # if we set self.batch_size = 0, we are asking to NOT use a mini-batch implementation
 		if(self.batch_size == 0):
-			# if we set self.batch_size = 0, we are asking to NOT use a mini-batch implementation
 			self.batch_size = self.datasets_class.get_num_collocation()
 		else:
-			# error if batch size is bigger than collocation size
 			if(self.batch_size > self.datasets_class.get_num_collocation()):
-				print("batch size: ",self.batch_size)
-				print("coll size: ",self.datasets_class.get_num_collocation())
+				print("batch size:", self.batch_size)
+				print( "coll size:", self.datasets_class.get_num_collocation())
 				raise Exception("Batch size can't be bigger than actual collocation size!")
 
 		coll_loader = data.batch(batch_size=self.batch_size)
 
-
-		return coll_loader,self.batch_size
+		return coll_loader, self.batch_size
 
 	def dataload_exact(self, exact_batch_size):
 		"""!
@@ -65,7 +55,7 @@ class dataloader:
 
 		@param exact_batch_size dimension of exact points batch"""
 		# get the exact (noisy) data
-		inputs,_,_ = self.datasets_class.get_exact_data_with_noise()
+		inputs, _, _ = self.datasets_class.get_exact_data_with_noise()
 
 		#load the data of collocation
 		data = tf.data.Dataset.from_tensor_slices(inputs)
@@ -75,7 +65,4 @@ class dataloader:
 							reshuffle_each_iteration=self.reshuffle_each_iteration)
 		exact_loader = data.batch(batch_size=exact_batch_size)
 
-		if(self.reshuffle_each_iteration):
-			print('Reshuffled at every epoch')
-
-		return exact_loader,exact_batch_size
+		return exact_loader, exact_batch_size
