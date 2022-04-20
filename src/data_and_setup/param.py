a0a6db8d553eba4016d75f565807414d0e7ec4bf
Giulia Mescolini <65012808+giuliamesc@users.noreply.github.com>
2022-04-20 22:37:48 +0200

    bug fix

--- src/data_and_setup/param.py
+++ src/data_and_setup/param.py
@@ -112,7 +112,7 @@ class Param:
             if not( isinstance(self.param_method["N_HMC"],int) and isinstance(self.param_method["M_HMC"],int)
                     and isinstance(self.param_method["L_HMC"],int) ):
                 raise TypeError("N_HMC, M_HMC and L_HMC must be integers")
-            if self.param_method["N_HMC"]<0 or self.param_method["M_HMC"]<0  or self.param_method["L_HMC"]:
+            if (self.param_method["N_HMC"]<0 or self.param_method["M_HMC"]<0  or self.param_method["L_HMC"]<0):
                 raise Exception(" N_HMC, M_HMC and L_HMC must be non-negative")
             if self.param_method["N_HMC"]<self.param_method["M_HMC"]:
                 raise Exception("M_HMC must be smaller than N_HMC")
