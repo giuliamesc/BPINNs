eab6a6e8e8312782d7eaa6c5c841e84b56c5fa06
Luca Sosta <lucasosta@libero.it>
2022-12-18 14:26:21 +0100

    td

--- src/main_data.py
+++ src/main_data.py
@@ -3,5 +3,4 @@ from utility import switch_dataset
 from setup import DataGenerator
 
 set_directory()
-#AnalyticalData(switch_dataset("Regression","cos"), do_plots=True, test_only=True, is_main=True).show_plot()
 DataGenerator(switch_dataset("Regression","cos"), False)
