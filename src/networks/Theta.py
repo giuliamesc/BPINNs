5af93fef91caf0b0b7b51bae361acd0dc6785907
Luca Sosta <lucasosta@libero.it>
2023-01-27 18:52:55 +0100

    refactor theta

--- src/networks/Theta.py
+++ src/networks/Theta.py
@@ -8,48 +8,56 @@ class Theta():
     """
     def __init__(self, theta): 
         self.values = theta
+
+    @property
+    def weights(self): 
+        """ Extraction of weights from self.values """
+        return self.values[0::2]
+    @property
+    def biases(self): 
+        """ Extraction of biases from self.values """
+        return self.values[1::2]
     
     def __mul__(self, other): 
         """ Multiplication of each Theta entry by a scalar (int or float) or elementwise multiplication of Thetas """
         if type(other) == int or type(other) == float: return Theta([a*other for a in self.values])
         return Theta([a*b for a,b in zip(self.values, other.values)])
-    def __neg__(self): 
-        """ Opposite of each Theta entry """
-        return self*(-1)
-    def __add__(self, other):
-        """ Addition of a scalar (int or float) to each Theta entry or elementwise sum of Thetas """
-        if type(other) == int or type(other) == float: return Theta([a+other for a in self.values])
-        return Theta([a+b for a,b in zip(self.values, other.values)])
-    def __sub__(self, other): 
-        """ Subtraction of a scalar (int or float) to each Theta entry or elementwise subtraction of Thetas """
-        return self+(-other)
-    def __truediv__(self, other) : 
-        """ Division of a Theta object by a scalar (int or float) or elementwise division of Thetas"""
-        return self*(other**(-1))
-    
-    def __rtruediv__(self, other): 
-        """ Division with the Theta object on the right """
-        return (self**(-1))*other
     def __rmul__(self, other): 
         """ Multiplication with the Theta object on the right """
         return self*other
-    def __radd__(self, other): 
-        """ Addition with the Theta object on the right """
-        return self+other
-    def __rsub__(self, other): 
-        """ Subtraction with the Theta object on the right """
-        return -self+other  
+    def __truediv__(self, other) : 
+        """ Division of a Theta object by a scalar (int or float) or elementwise division of Thetas"""
+        return self*(other**(-1))
+    def __rtruediv__(self, other): 
+        """ Division with the Theta object on the right """
+        return (self**(-1))*other
     
+    def __neg__(self): 
+        """ Opposite of each Theta entry """
+        return self*(-1)
     def __pow__(self, exp): 
         """ Implementation of elementwise square, sqrt and reciprocal of a Theta object """
         if exp == 2:   return Theta([tf.math.square(t)     for t in self.values])
         if exp == 0.5: return Theta([tf.math.sqrt(t)       for t in self.values])
         if exp == -1:  return Theta([tf.math.reciprocal(t) for t in self.values])
+    
+    def __add__(self, other):
+        """ Addition of a scalar (int or float) to each Theta entry or elementwise sum of Thetas """
+        if type(other) == int or type(other) == float: return Theta([a+other for a in self.values])
+        return Theta([a+b for a,b in zip(self.values, other.values)])
+    def __radd__(self, other): 
+        """ Addition with the Theta object on the right """
+        return self+other
+    def __sub__(self, other): 
+        """ Subtraction of a scalar (int or float) to each Theta entry or elementwise subtraction of Thetas """
+        return self+(-other)
+    def __rsub__(self, other): 
+        """ Subtraction with the Theta object on the right """
+        return -self+other  
 
     def __len__(self): 
         """ Returns the number of tensors in the self.values list """
         return len(self.values)
-    
     def __str__(self):
         """ Formatted print of a Theta object """
         for i, (w,b) in enumerate(zip(self.weights, self.biases)):
@@ -69,12 +77,3 @@ class Theta():
     def normal(self, std): 
         """ Creation of a Theta object with random normal initialization of self.values """
         return Theta([tf.random.normal(t.shape, stddev=std) for t in self.values]) 
-
-    @property
-    def weights(self): 
-        """ Extraction of weights from self.values """
-        return self.values[0::2]
-    @property
-    def biases(self): 
-        """ Extraction of biases from self.values """
-        return self.values[1::2]
